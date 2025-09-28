d301ea9e429d251a33ac77dee3bdba4421c874be
pokeyugiweb <pokeyugiweb@gmail.com>
2025-09-27 23:13:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+
+function dueloOffline() {
+  alert("Modo Duelo Offline activado. ¡Prepárate para invocar!");
+}
+
+function dueloOnline() {
+  alert("Conectando al servidor... ¡Duelo en línea pronto disponible!");
+}
+
+function editMazo() {
+  alert("Editor de mazo abierto. ¡Organiza tus cartas como un verdadero duelista!");
+}
+
+function ruleta() {
+  alert("Girando la ruleta... ¿Qué carta te tocará?");
+}
+
+function acerca() {
+  alert("Pokeyugi es un juego web creado por MisterTraiding. ¡Versión pixel retro con cartas personalizadas!");
+}
+
+function opciones() {
+  alert("Opciones disponibles próximamente. ¡Ajusta tu experiencia de duelo!");
+}
+
+function salir() {
+  alert("Gracias por jugar Pokeyugi. ¡Nos vemos en el próximo duelo!");
+}
